d3d2e2ba1eeb4c220d0da25d0e360ab3a7bbde93
sahilraj-001x <84084455+sahilraj-001x@users.noreply.github.com>
2021-05-20 16:35:00 +0530

    Update students.py

--- students.py
+++ students.py
@@ -1,6 +1,7 @@
 student_one = {
     "student_name": "rohit meheta",
     "student_age": 19,
+    "student_password" : "123abc",
     "student_subject_marks" :{
         "Maths" : 89,
         "English" : 91,
@@ -13,6 +14,7 @@ student_one = {
 student_two = {
     "student_name": "sanjog bahara",
     "student_age": 20,
+    "student_password" : "123def",
     "student_subject_marks" :{
         "Biology" : 70,
         "English" : 95,
@@ -25,6 +27,7 @@ student_two = {
 student_three = {
     "student_name": "antoneo zenefer",
     "student_age": 19,
+    "student_password" : "123ghi",
     "student_subject_marks" :{
         "Maths" : 93,
         "English" : 82,
@@ -37,6 +40,7 @@ student_three = {
 student_four = {
     "student_name": "natasha romonoff",
     "student_age": 21,
+    "student_password" : "456abc",
     "student_subject_marks" :{
         "Biology" : 94,
         "English" : 97,
@@ -49,6 +53,7 @@ student_four = {
 student_five = {
     "student_name": "devis jarvis",
     "student_age": 19,
+    "student_password" : "456def",
     "student_subject_marks" :{
         "Maths" : 99,
         "English" : 87,
@@ -59,8 +64,9 @@ student_five = {
 }
 
 student_six = {
-"student_name": "mark portfollio",
+    "student_name": "mark portfollio",
     "student_age": 21,
+    "student_password" : "456ghi",
     "student_subject_marks" :{
         "Political Science" : 64,
         "English" : 86,
@@ -73,10 +79,11 @@ student_six = {
 student_seven = {
     "student_name": "tony stark",
     "student_age": 18,
+    "student_password" : "789abc",
     "student_subject_marks" :{
         "Maths" : 100,
         "English" : 91,
-        "Physics" : 100,
+        "Physics" : 98,
         "Chemistry" :91,
         "Cse" : 100
     }
@@ -85,6 +92,7 @@ student_seven = {
 student_eight = {
     "student_name": "donald michelle",
     "student_age": 20,
+    "student_password" : "789def",
     "student_subject_marks" :{
         "Biology" : 98,
         "Geography" : 71,
@@ -97,6 +105,7 @@ student_eight = {
 student_nine = {
     "student_name": "shreya aggarwal",
     "student_age": 20,
+    "student_password" : "789ghi",
     "student_subject_marks" :{
         "Biology" : 91,
         "English" : 80,
@@ -109,6 +118,7 @@ student_nine = {
 student_ten = {
     "student_name": "lamina heignsberg",
     "student_age": 19,
+    "student_password" : "123jkl",
     "student_subject_marks" :{
         "Maths" : 75,
         "English" : 93,
@@ -121,6 +131,7 @@ student_ten = {
 student_eleven = {
     "student_name": "oman petersberg",
     "student_age": 21,
+    "student_password" : "456jkl",
     "student_subject_marks" :{
         "Maths" : 99,
         "Civil" : 83,
@@ -133,6 +144,7 @@ student_eleven = {
 student_twelve = {
     "student_name": "daniel clifcurt",
     "student_age": 19,
+    "student_password" : "789jkl",
     "student_subject_marks" :{
         "Maths" : 89,
         "English" : 91,
@@ -141,4 +153,3 @@ student_twelve = {
         "Cse" : 88
     }
 }
-
